b39ab3143e8e3cda1dd38dec0d3f7dad84544f69
Michal Kalita <kalita.michal@gmail.com>
2018-09-18 21:19:33 +0200

    Created websocket server, broadcasting messages

--- src/App.js
+++ src/App.js
@@ -17,13 +17,39 @@ class App extends Component {
     }
 
     this.sendMessage = this.sendMessage.bind(this)
+
+    var W3CWebSocket = require('websocket').w3cwebsocket;
+
+    const client = new W3CWebSocket('ws://localhost:8080/', 'echo-protocol');
+
+    client.onerror = function() {
+        console.log('Connection Error');
+    };
+
+    client.onopen = function() {
+        console.log('WebSocket Client Connected');
+    };
+
+    client.onclose = function() {
+        console.log('echo-protocol Client Closed');
+    };
+
+    client.onmessage = function(e) {
+        if (typeof e.data === 'string') {
+            console.log("Received: '" + e.data + "'");
+            this.setState({
+              messages: [...this.state.messages, e.data]
+            })
+        }
+    }.bind(this);
+    this.client = client
   }
 
   sendMessage() {
     const message = this.state.actualValue
+    this.client.send(message)
     this.setState({
-      actualValue: '',
-      messages: [...this.state.messages, message]
+      actualValue: ''
     })
   }
 
